a2ac590e08d68389fd857a8a755a30a16b03b398
Robert Hensing <robert@roberthensing.nl>
2021-10-07 09:58:04 +0200

    Use separate nixpkgs for nixops 1 for now

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   description = "Hercules CI Effects";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  # TODO remove when nixpkgs#138584 lands
+  inputs.nixpkgs-nixops.url = "github:NixOS/nixpkgs/8d8a28b47b7c41aeb4ad01a2bd8b7d26986c3512";
   inputs.flake-compat.url = "github:edolstra/flake-compat/master";
   inputs.flake-compat.flake = false;
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-nixops, ... }: {
 
     lib.withPkgs = pkgs:
       let effects = import ./effects/default.nix effects pkgs;
@@ -19,7 +21,7 @@
 
     tests = {
       git-crypt-hook = nixpkgs.legacyPackages.x86_64-linux.callPackage ./effects/git-crypt-hook/test.nix {};
-      nixops = nixpkgs.legacyPackages.x86_64-linux.callPackage ./effects/nixops/test/default.nix {};
+      nixops = nixpkgs-nixops.legacyPackages.x86_64-linux.callPackage ./effects/nixops/test/default.nix {};
       nix-shell = nixpkgs.legacyPackages.x86_64-linux.callPackage ./effects/nix-shell/test.nix {};
       nixops2 = nixpkgs.legacyPackages.x86_64-linux.callPackage ./effects/nixops2/test/default.nix { nixpkgsFlake = nixpkgs; };
     };
