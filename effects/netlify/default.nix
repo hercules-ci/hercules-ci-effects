754d1c7b1b65c8ad21ed09ce0982eff8bd939d4b
Robert Hensing <robert@roberthensing.nl>
2022-05-24 18:02:50 +0200

    netlifyDeploy: Simplify

--- effects/netlify/default.nix
+++ effects/netlify/default.nix
@@ -2,12 +2,10 @@
 , mkEffect
 , netlify-cli
 }:
-args@
-{ content ? websitePackage
-, websitePackage ? throw "effects.netlify: You must provide the `content` parameter, which is a directory to deploy."
-, secretName ? throw ''effects.netlify: You must provide `secretName` the name of the secret which holds the "${secretField}" field.''
-, secretData ? "token"
-, secretField ? secretData
+
+{ content
+, secretName ? throw ''effects.netlify: You must provide `secretName`, the name of the secret which holds the "${secretField}" field.''
+, secretField ? "token"
 , siteId
 , productionDeployment ? false
 }:
@@ -17,8 +15,6 @@ let
     "--site=${siteId}"
   ] ++ lib.optionals productionDeployment [ "--prod" ];
 in
-lib.warnIf (args?websitePackage) "effects.netlify: Use the `content` parameter instead of `websitePackage`."
-lib.warnIf (args?secretData) ''effects.netlify: Use the `secretField` parameter instead of `secretData`, or rely on the default field name: "token".''
 mkEffect {
   inputs = [ netlify-cli ];
   secretsMap."netlify" = secretName;
