6e024bce1c5f9f4855c95bb5827ee32e33295c57
Robert Hensing <robert@roberthensing.nl>
2022-11-06 15:38:27 +0100

    netlifyDeploy: Install credentials in user setup phase

--- effects/netlify/default.nix
+++ effects/netlify/default.nix
@@ -1,6 +1,7 @@
 { lib
 , mkEffect
 , netlify-cli
+, netlifySetupHook
 }:
 
 args@{ content
@@ -22,38 +23,11 @@ let
   ] ++ extraDeployArgs;
 in
 mkEffect (args // {
+  buildInputs = [ netlifySetupHook ];
   inputs = [ netlify-cli ];
   secretsMap = { "netlify" = secretName; } // secretsMap;
+  netlifySecretField = secretField;
   effectScript = ''
-
-    # Install the token
-    #
-    # Netlify does not offer (or perhaps not document)
-    # an alternate method of passing the token other than
-    # the --auth flag, which is insecure. So we reverse
-    # engineer the config file.
-    # This effect can be tested with
-    #
-    #     nix develop
-    #     hci effect run default.effects.tests.netlifyDeploy
-    #
-    mkdir -p ~/.config/netlify
-    cat >~/.config/netlify/config.json <<EOF
-    { "userId": "effects-netlifyDeploy-unknown-user-id"
-    , "users": {
-        "effects-netlifyDeploy-unknown-user-id": {
-          "id": "effects-netlifyDeploy-unknown-user-id",
-          "name": "effects.netlifyDeploy",
-          "email": "effects.netlifyDeploy@example.com",
-          "auth": {
-            "token": "$(readSecretString netlify .${secretField})",
-            "github": {}
-          }
-        }
-      }
-    }
-    EOF
-
     netlify deploy \
       ${lib.escapeShellArgs deployArgs} | tee netlify-result.json
     # netlify does not print a newline after the json output, so we add it to keep the log tidy
