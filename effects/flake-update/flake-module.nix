8be09f343d3d0c97ea3983e1a3eed826ddda6a40
Ilan Joselevich <personal@ilanjoselevich.com>
2022-11-15 19:07:35 +0200

    flakeUpdate: sync module with function

--- effects/flake-update/flake-module.nix
+++ effects/flake-update/flake-module.nix
@@ -4,34 +4,53 @@ let
   cfg = config.hercules-ci.flake-update;
 in
 {
-  options = {
-    hercules-ci.flake-update = {
-      enable = lib.mkEnableOption "Scheduled flake update job";
-      updateBranch = mkOption {
-        type = types.str;
-        default = "flake-update";
-        example = "update";
-        description = ''
-          To which branch to push the updated flake lock.
-        '';
-      };
-      when = mkOption {
-        type = types.raw;
-        description = ''
-          See `herculesCI.onSchedule.<name>.when` for details.
-        '';
-      };
-      effect.system = mkOption {
-        type = types.str;
-        default = config.defaultEffectSystem;
-        defaultText = lib.literalExpression "config.defaultEffectSystem";
-        example = "aarch64-linux";
-        description = ''
-          The [system](https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-system) on which to run the flake update job.
-        '';
-      };
+  options.hercules-ci.flake-update = {
+    enable = lib.mkEnableOption "Scheduled flake update job";
+
+    updateBranch = mkOption {
+      type = types.str;
+      default = "flake-update";
+      example = "update";
+      description = ''
+        To which branch to push the updated flake lock.
+      '';
+    };
+
+    forgeType = mkOption {
+      type = types.str;
+      default = "github";
+      example = "gitea";
+      description = ''
+        The type of Git server commited to.
+      '';
+    };
+
+    createPullRequest = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Whether to create a pull request for the updated flake.lock.
+      '';
+    };
+
+    when = mkOption {
+      type = types.raw;
+      description = ''
+        See `herculesCI.onSchedule.<name>.when` for details.
+      '';
+    };
+
+    effect.system = mkOption {
+      type = types.str;
+      default = config.defaultEffectSystem;
+      defaultText = lib.literalExpression "config.defaultEffectSystem";
+      example = "aarch64-linux";
+      description = ''
+        The [system](https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-system) on which to run the flake update job.
+      '';
     };
   };
+
   config = {
     herculesCI = herculesCI@{ config, ... }: optionalAttrs (cfg.enable) {
       onSchedule.flake-update = {
@@ -42,7 +61,7 @@ in
               hci-effects.flakeUpdate {
                 gitRemote = herculesCI.config.repo.remoteHttpUrl;
                 user = "x-access-token";
-                inherit (cfg) updateBranch;
+                inherit (cfg) updateBranch forgeType createPullRequest;
               }
             );
           };
