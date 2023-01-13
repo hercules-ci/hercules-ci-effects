358e325faca757f13a74377efb76c3959bd4e2e6
Robert Hensing <robert@roberthensing.nl>
2023-01-13 21:18:35 +0100

    Update flake-parts mkFlake invocations

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.hercules-ci-agent.url = "hercules-ci-agent";
 
-  outputs = { self, nixpkgs, hercules-ci-agent, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit self; }
+  outputs = inputs@{ self, nixpkgs, hercules-ci-agent, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
       ({ withSystem, ... }: {
         imports = [
           ./flake-public-outputs.nix
