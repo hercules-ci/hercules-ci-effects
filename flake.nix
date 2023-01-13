39bff9120544e978eda25de95343866f5026dd35
Robert Hensing <robert@roberthensing.nl>
2023-01-13 21:16:13 +0100

    flake.nix: Update
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/0e101dbae756d35a376a5e1faea532608e4a4b9a' (2022-10-27)
      → 'github:hercules-ci/flake-parts/82c16f1682cf50c01cb0280b38a1eed202b3fe9f' (2023-01-10)
    • Updated input 'flake-parts/nixpkgs-lib':
        'github:NixOS/nixpkgs/34c5293a71ffdb2fe054eb5288adc1882c1eb0b1?dir=lib' (2022-10-09)
      → 'github:NixOS/nixpkgs/677ed08a50931e38382dbef01cba08a8f7eac8f6?dir=lib' (2022-12-29)
    • Updated input 'hercules-ci-agent':
        'github:hercules-ci/hercules-ci-agent/58954ab190bbc27a9d5149ebdafbf80851476461' (2022-11-09)
      → 'github:hercules-ci/hercules-ci-agent/b3f8aa8e4a8b22dbbe92cc5a89e6881090b933b3' (2023-01-08)
    • Updated input 'hercules-ci-agent/flake-parts':
        'github:hercules-ci/flake-parts/608ed3502263d6f4f886d75c48fc2b444a4ab8d8' (2022-07-06)
      → 'github:hercules-ci/flake-parts/0e101dbae756d35a376a5e1faea532608e4a4b9a' (2022-10-27)
    • Removed input 'hercules-ci-agent/flake-parts/nixpkgs'
    • Added input 'hercules-ci-agent/flake-parts/nixpkgs-lib':
        follows 'hercules-ci-agent/nixpkgs'
    • Updated input 'hercules-ci-agent/nix-darwin':
        'github:LnL7/nix-darwin/54a24f042f93c79f5679f133faddedec61955cf2' (2022-07-14)
      → 'github:LnL7/nix-darwin/cfc0125eafadc9569d3d6a16ee928375b77e3100' (2022-11-02)
    • Updated input 'hercules-ci-agent/nixpkgs':
        'github:NixOS/nixpkgs/c4a0efdd5a728e20791b8d8d2f26f90ac228ee8d' (2022-08-12)
      → 'github:NixOS/nixpkgs/e182da8622a354d44c39b3d7a542dc12cd7baa5f' (2022-12-28)
    • Updated input 'hercules-ci-agent/pre-commit-hooks-nix':
        'github:hercules-ci/pre-commit-hooks.nix/596dac761042d9ba90a507d43ad506cb952c984d' (2022-05-29)
      → 'github:cachix/pre-commit-hooks.nix/06f48d63d473516ce5b8abe70d15be96a0147fcd' (2022-11-06)
    • Updated input 'hercules-ci-agent/pre-commit-hooks-nix/flake-utils':
        'github:numtide/flake-utils/2ebf2558e5bf978c7fb8ea927dfaed8fefab2e28' (2021-04-25)
      → 'github:numtide/flake-utils/6ee9ebb6b1ee695d2cacc4faa053a7b9baa76817' (2022-10-29)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/52392d42c156db5b889db7f3cc3e9909e4259b2a' (2022-09-28)
      → 'github:NixOS/nixpkgs/6f6b4a1d4175d3f6fb72dbaceea8d6e4e79c5d6e' (2023-01-13)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Hercules CI Effects";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.hercules-ci-agent.url = "hercules-ci-agent/on-schedule";
+  inputs.hercules-ci-agent.url = "hercules-ci-agent";
 
   outputs = { self, nixpkgs, hercules-ci-agent, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit self; }
