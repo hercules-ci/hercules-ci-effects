323dbea9faa2459b5474f72723b9103dc56cffc1
Robert Hensing <robert@roberthensing.nl>
2023-01-27 15:34:49 +0100

    Add hercules-ci.github-pages to flake-parts module

--- flake-module.nix
+++ flake-module.nix
@@ -3,6 +3,7 @@
     ./flake-modules/module-argument.nix
     ./flake-modules/herculesCI-attribute.nix
     ./flake-modules/herculesCI-helpers.nix
+    ./flake-modules/github-pages.nix
     ./effects/flake-update/flake-module.nix
   ];
 }
---
+++ flake-modules/github-pages.nix
@@ -0,0 +1,135 @@
+top@{ config, options, lib, inputs, withSystem, getSystem, flake-parts-lib, ... }:
+let
+  inherit (lib)
+    mkIf
+    mkMerge
+    mkOption
+    types
+    ;
+
+  inherit (config) defaultEffectSystem;
+  inherit (flake-parts-lib) mkPerSystemOption;
+
+  cfg = config.hercules-ci.github-pages;
+
+  enable = cfg.branch != null;
+
+  githubPagesSettings = {
+    _file = "${__curPos.file}:let githubPagesSettings";
+    git.checkout.forgeType = "github";
+    git.checkout.user = "x-access-token";
+    git.update.branch = "gh-pages";
+  };
+
+in
+{
+  options = {
+    perSystem = mkPerSystemOption ({ config, ... }: {
+      options = {
+        hercules-ci.github-pages.settings = lib.mkOption {
+          type = types.deferredModule;
+          description = ''
+            Modular settings for the GitHub Pages effect.
+
+            See [`gitWriteBranch`](https://docs.hercules-ci.com/hercules-ci-effects/reference/nix-functions/gitWriteBranch.html#effect_options) for options.
+          '';
+          example = lib.literalExpression ''
+            {
+              contents = config.packages.docs + "/share/doc/mypkg/html";
+            }
+          '';
+        };
+      };
+    });
+
+    hercules-ci.github-pages = {
+      branch = lib.mkOption {
+        type = types.nullOr types.str;
+        description = ''
+          A GitHub Pages deployment will be triggered when changes are pushed to this branch.
+
+          A non-null value enables the effect.
+        '';
+        default = null;
+      };
+      check.enable = lib.mkOption {
+        type = types.bool;
+        default = true;
+        description = ''
+          Whether to make sure that the effect is buildable. This adds
+          `checks.''${config.defaultEffectSystem}.gh-pages` to `onPush.default`.
+        '';
+      };
+      pushJob = lib.mkOption {
+        type = types.str;
+        description = ''
+          The Hercules CI job in which to perform the deployment.
+          
+          By default the GitHub pages deployment is triggered by the `onPush.default` job,
+          so that the deployment only proceeds when the default builds are successful.
+        '';
+        default = "default";
+      };
+      settings = lib.mkOption {
+        type = types.deferredModule;
+        description = ''
+          Modular settings for the GitHub Pages effect.
+
+          For system-dependent settings, define [`perSystem.hercules-ci.github-pages.settings`](#opt-perSystem.hercules-ci.github-pages.settings) instead.
+
+          See [`gitWriteBranch`](https://docs.hercules-ci.com/hercules-ci-effects/reference/nix-functions/gitWriteBranch.html#effect_options) for options.
+        '';
+        example = lib.literalExpression ''
+          {
+            message = "Update GitHub Pages";
+          }
+        '';
+      };
+    };
+  };
+
+  config = mkIf enable {
+    perSystem = { hci-effects, system, ... }:
+      let
+        deploy =
+          hci-effects.gitWriteBranch {
+            imports = [
+              githubPagesSettings
+              cfg.settings
+            ];
+            git.checkout.remote.url = "https://fake-repo-for.checks.github-pages-effect-is-buildable";
+          };
+      in
+      {
+        checks = lib.optionalAttrs (system == defaultEffectSystem) {
+          github-pages-effect-is-buildable = deploy.inputDerivation;
+        };
+      };
+
+    hercules-ci.github-pages.settings = (getSystem defaultEffectSystem).hercules-ci.github-pages.settings;
+
+    herculesCI = herculesCI@{ config, ... }:
+      let
+        deploy = withSystem defaultEffectSystem ({ config, hci-effects, ... }:
+          hci-effects.gitWriteBranch {
+            imports = [
+              githubPagesSettings
+              cfg.settings
+            ];
+            git.checkout.remote.url = herculesCI.config.repo.remoteHttpUrl;
+          }
+        );
+      in
+      {
+        onPush = mkMerge [
+          # deploy
+          {
+            ${cfg.pushJob}.outputs.effects.gh-pages =
+              lib.optionalAttrs
+                (herculesCI.config.repo.branch == cfg.branch)
+                deploy;
+          }
+        ];
+      };
+  };
+}
