736e1d7ebee699362c3d93bef0c4660b41650402
Robert Hensing <robert@roberthensing.nl>
2023-01-27 11:37:42 +0100

    doc: Add git module docs and improve rendering

--- flake-docs-render.nix
+++ flake-docs-render.nix
@@ -1,6 +1,33 @@
 {
-  perSystem = { config, pkgs, lib, ... }: {
-    packages.generated-option-doc-modularEffect =
+  perSystem = { config, pkgs, lib, ... }:
+  let
+    inherit (lib)
+      concatMap
+      hasPrefix
+      removePrefix
+      ;
+
+    filterTransformOptions = { sourceName, sourcePath, baseUrl }:
+      let sourcePathStr = toString sourcePath;
+      in
+      opt:
+      let
+        declarations = concatMap
+          (decl:
+            if hasPrefix sourcePathStr (toString decl)
+            then
+              let subpath = removePrefix sourcePathStr (toString decl);
+              in [{ url = baseUrl + subpath; name = sourceName + subpath; }]
+            else [ ]
+          )
+          opt.declarations;
+      in
+      if declarations == [ ]
+      then opt // { visible = false; }
+      else opt // { inherit declarations; };
+
+  
+    renderModule = { sourceName, sourcePath, modules }:
       # TODO: use the render pipeline in flake-parts,
       #       which has support for things like {options}`foo`.
       let
@@ -12,28 +39,78 @@
                 # type = lib.types.submodule;
               };
             }
-            ./effects/effect/effect-module.nix
-          ];
+            ./effects/effect/effect-module.nix 
+          ] ++ modules;
         };
+        baseUrl = 
+          "https://github.com/hercules-ci/hercules-ci-effects/blob/master" 
+            + lib.strings.removePrefix (toString ./.) (toString sourcePath);
+
       in
       (pkgs.nixosOptionsDoc
         {
           options = eval.options;
-        }).optionsCommonMark;
+          transformOptions = filterTransformOptions {
+            inherit sourceName baseUrl sourcePath;
+          };
+          documentType = "none";
+          warningsAreErrors = true;
+          markdownByDefault = true;
+        }).optionsDocBook;
+  in
+  {
+    packages.generated-option-doc-modularEffect =
+      renderModule {
+        sourceName = "effect";
+        sourcePath = ./effects/effect;
+        modules = [ ];
+      };
+
+    packages.generated-option-doc-git-auth =
+      renderModule {
+        sourceName = "git-auth";
+        sourcePath = ./effects/modules/git-auth.nix;
+        modules = [ ./effects/modules/git-auth.nix ];
+      };
+
+    packages.generated-option-doc-git-update =
+      renderModule {
+        sourceName = "git-auth";
+        sourcePath = ./effects/modules/git-update.nix;
+        modules = [ ./effects/modules/git-update.nix ];
+      };
+
 
     packages.generated-antora-files =
       pkgs.runCommand "generated-antora-files"
         {
-          nativeBuildInputs = [ pkgs.pandoc ];
+          nativeBuildInputs = [ pkgs.pandoc pkgs.libxslt.bin ];
           modularEffect = config.packages.generated-option-doc-modularEffect;
+          git_auth = config.packages.generated-option-doc-git-auth;
+          git_update = config.packages.generated-option-doc-git-update;
         }
-        # TODO: use the render pipeline in flake-parts,
-        #       which has support for things like {options}`foo`.
         ''
-          mkdir -p $out/modules/ROOT/partials
-          pandoc --from=markdown --to=asciidoc \
+          mkdir -p $out/modules/ROOT/partials/options
+
+          function convert() {
+            xsltproc --stringparam title Options \
+              -o options.db.xml ${./options.xsl} \
+              -
+            pandoc --from=docbook --to=asciidoc \
+              < options.db.xml\
+            | sed -e 's/^ *//'  -e 's/^== *$//'
+          }
+          convert \
             < $modularEffect \
-            > $out/modules/ROOT/partials/options.adoc
+            >$out/modules/ROOT/partials/options.adoc \
+
+          convert \
+            < $git_auth \
+            > $out/modules/ROOT/partials/options/git-auth.adoc
+
+           convert\
+            < $git_update \
+            > $out/modules/ROOT/partials/options/git-update.adoc
         '';
   };
 }
