fb1d65f7a0619f606aa264412e641fc94c61fadc
Robert Hensing <robert@roberthensing.nl>
2023-11-25 12:10:55 +0100

    test: Add checks.flake-update-nix-unstable

--- flake-dev.nix
+++ flake-dev.nix
@@ -90,6 +90,8 @@ top@{ withSystem, lib, inputs, config, self, ... }: {
           );
     in {
       flake-update = hci-effects.callPackage ./effects/flake-update/test.nix { };
+      # TODO after https://github.com/NixOS/nix/issues/7730, use nix master
+      flake-update-nix-unstable = hci-effects.callPackage ./effects/flake-update/test.nix { nix = pkgs.nixVersions.unstable; };
       git-update = hci-effects.callPackage ./effects/modules/git-update/test.nix { };
       write-branch = hci-effects.callPackage ./effects/write-branch/test.nix { };
       ssh = hci-effects.callPackage ./effects/ssh/test.nix { };
